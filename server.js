10ad162bcab81c94d3223d08010caaf650c2d227
Aurora <a.borealisdesigns@gmail.com>
2026-04-17 10:01:45 -0400

    again

--- server.js
+++ server.js
@@ -36,16 +36,16 @@ const game = {
 
 // CHARACTER STORAGE
 const characters = [
-    { name: "Joe the Elf", front: "/characters/fancydancerblack.png", back: "/characters/joetheelf.png" },
+    { name: "Joe the Elf", front: "/characters/tricerexfront.png", back: "/characters/joetheelf.png" },
     { name: "Tricerex", front: "/characters/tricerexfront.png", back: "/characters/tricerex.png" },
     { name: "Deerhead", front: "/characters/deerheadfront.png", back: "/characters/deerhead.png" },
-    { name: "Janice Mowes", front: "/characters/fancydancerblack.png", back: "/characters/janicemowes.png" },
-    { name: "Old Sawyer", front: "/characters/fancydancerblack.png", back: "/characters/oldsawyer.png" },
-    { name: "Jesus", front: "/characters/fancydancerblack.png", back: "/characters/jesus.png" },
+    { name: "Janice Mowes", front: "/characters/tricerexfront.png", back: "/characters/janicemowes.png" },
+    { name: "Old Sawyer", front: "/characters/tricerexfront.png", back: "/characters/oldsawyer.png" },
+    { name: "Jesus", front: "/characters/tricerexfront.png", back: "/characters/jesus.png" },
     { name: "Fancy Dancer", front: "/characters/fancydancerpinkfront.png", back: "/characters/fancydancerpink.png" },
-    { name: "Donna", front: "/characters/fancydancerblack.png", back: "/characters/donna.png" },
-    { name: "Lorenzo", front: "/characters/fancydancerblack.png", back: "/characters/lorenzo.png" },
-    { name: "Caity Satyr", front: "/characters/fancydancerblack.png", back: "/characters/caitysatyr.png" },
+    { name: "Donna", front: "/characters/tricerexfront.png", back: "/characters/donna.png" },
+    { name: "Lorenzo", front: "/characters/tricerexfront.png", back: "/characters/lorenzo.png" },
+    { name: "Caity Satyr", front: "/characters/tricerexfront.png", back: "/characters/caitysatyr.png" },
     { name: "The Boss", front: "/characters/thebossfront.png", back: "/characters/theboss.png" }
 ];
 
