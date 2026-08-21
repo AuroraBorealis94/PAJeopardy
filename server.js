9302c5dff850d2191d0d7c20bda0e48eeb86aaff
Aurora Borealis <a.borealisdesigns@gmail.com>
2026-08-21 09:53:12 -0400

    Gamestate bug, lobby btn id/onclick

--- server.js
+++ server.js
@@ -850,16 +850,15 @@ io.on("connection", (socket) => {
         console.log("HOST ACTION:", data);
 
         // HOST STATE ACTIONS
-        if (data.type === "startGame") {
+        if (data.type === "enableLobby") {
             console.log("HOST STARTING ROUND 1");
             game.state = "playing";
             game.round = 1;
             game.hostScreen = "hostLobbyPg";
             generateBoard(1);
 
-            broadcastToUnity({ type: "startGame", round: game.round });
+            broadcastToUnity({ type: "enableLobby", round: game.round });
             io.emit("roundStarted", { round: game.round });
-            //io.emit("showInstructionsHolding");
 
             setTimeout(() => {
                 const boardData = {
